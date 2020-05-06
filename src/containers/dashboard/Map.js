402740646b3e7ceb6cd90ed71c0a6085375551d1
Melanie <melanie.mazanec@gmail.com>
2020-05-06 19:22:14 -0400

    Simplify color handling

--- src/containers/dashboard/Map.js
+++ src/containers/dashboard/Map.js
@@ -1,79 +1,62 @@
-import React, { Component } from "react";
-import DistrictsMap from "./districts_map.svg";
-import Mapv2OverLay from "./Mapv2Overlay";
-import { SvgLoader, SvgProxy } from "react-svgmt";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import "./Map.css";
+import React, { Component } from 'react';
+import { scaleLinear } from 'd3-scale';
+import { color } from 'd3-color';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { SvgLoader, SvgProxy } from 'react-svgmt';
+import DistrictsMap from './districts_map.svg';
+import Mapv2OverLay from './Mapv2Overlay';
+import './Map.css';
 
-class Mapv2 extends Component {
-  handleSelectDistrict(district) {
-    this.props.selectDistrict(district);
-  }
-  genShortagesColor(rate) {
-    //Calculates the color of each district based on the rate of respondents; could be less verbose by using an array method; consider refactoring
-    if (rate >= 0 && rate <= 25) {
-      return "#f8e9bf";
-    } else if (rate > 25 && rate <= 50) {
-      return "#dba487";
-    } else if (rate > 50 && rate <= 75) {
-      return "#ba605d";
-    } else if (rate > 75 && rate <= 100) {
-      return "#94003a";
-    }
-  }
-  genTestingColor(rate) {
-    //Calculates the color of each district based on the rate of respondents; could be less verbose by using an array method; consider refactoring
-    if (rate >= 0 && rate <= 25) {
-      return "#f2e1f5";
-    } else if (rate > 25 && rate <= 50) {
-      return "#c990d6";
-    } else if (rate > 50 && rate <= 75) {
-      return "#9445ab";
-    } else if (rate > 75 && rate <= 100) {
-      return "#500b65";
-    }
-  }
-  render() {
-    const { dataByDistrict, categoryDisplay } = this.props;
-    console.log(dataByDistrict)
-    return (
-      <div className="DistrictMaps_Container">
-        <TransformWrapper
-          defaultScale={1}
-          defaultPositionX={200}
-          defaultPositionY={100}
-        >
-          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-            <React.Fragment>
-              <TransformComponent>
-                <SvgLoader path={DistrictsMap}>
-                  <SvgProxy selector={"path"} fill="black" />
-                  {dataByDistrict.map((data) => (
-                    <SvgProxy
-                      key={`#${data.district}`}
-                      selector={`#${data.district}`}
-                      fill={
-                        categoryDisplay === 0
-                          ? this.genShortagesColor(data.shortagesRate)
-                          : this.genTestingColor(data.noTestingRate)
-                      }
-                      onClick={() => this.handleSelectDistrict(data)}
-                    />
-                  ))}
-                </SvgLoader>
-              </TransformComponent>
+class Map extends Component {
+	handleSelectDistrict(district) {
+		this.props.selectDistrict(district);
+	}
+	genColor(saturatedColor, rate) {
+		const rgbColor = scaleLinear().domain([0, 100]).range(['#fff', saturatedColor])(rate || 0);
+		const hexColor = color(rgbColor).formatHex();
+		return hexColor;
+	}
+	render() {
+		const { dataByDistrict, categoryDisplay } = this.props;
 
-              <Mapv2OverLay
-                categoryDisplay={categoryDisplay}
-                zoomIn={zoomIn}
-                zoomOut={zoomOut}
-              />
-            </React.Fragment>
-          )}
-        </TransformWrapper>
-      </div>
-    );
-  }
+    // TODO: this is navy like the mockups-- is that what we want?
+    const saturatedColor = '#303a84';
+
+		// let saturatedColor = '#500b65';
+		let rateAccessor = 'noTestingRate';
+
+		if (categoryDisplay === 0) {
+			// saturatedColor = '#94003a';
+			rateAccessor = 'shortagesRate';
+		}
+
+		return (
+			<div className="DistrictMaps_Container">
+				<TransformWrapper defaultScale={1} defaultPositionX={200} defaultPositionY={100}>
+					{({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+						<React.Fragment>
+							<TransformComponent>
+								<SvgLoader path={DistrictsMap}>
+									<SvgProxy selector={'path'} fill="white" />
+									{dataByDistrict.map((data) => (
+										<SvgProxy
+											key={data.district}
+											selector={data.district}
+											// fill={this.genColor(saturatedColor, data[rateAccessor] || 0)}
+											fill="#fff"
+											onClick={() => this.handleSelectDistrict(data)}
+										/>
+									))}
+								</SvgLoader>
+							</TransformComponent>
+
+							<Mapv2OverLay categoryDisplay={categoryDisplay} zoomIn={zoomIn} zoomOut={zoomOut} />
+						</React.Fragment>
+					)}
+				</TransformWrapper>
+			</div>
+		);
+	}
 }
 
-export default Mapv2;
+export default Map;
