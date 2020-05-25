ff04da82ab9c5f35be88a46f1d31897d5f3ee4e3
Melanie <melanie.mazanec@gmail.com>
2020-05-25 14:48:58 -0400

    Move about Code for Dayton text into a component and correct typo

---
+++ src/assets/utils/AboutCodeForDayton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+	<p>
+		<strong>Flo's Whistle: Pandemic</strong> is a Code for Dayton project. We're an independent
+		collaboration of volunteer developers, nurses, physicians, attorneys, writters, designers, and
+		journalists from the Code for America Brigade community. ©2020
+	</p>
+);
--- src/containers/thankyoupage/ThankYouPage.js
+++ src/containers/thankyoupage/ThankYouPage.js
@@ -1,49 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./ThankYouPage.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AboutCodeForDayton from '../../assets/utils/AboutCodeForDayton';
+import './ThankYouPage.css';
 
 const ThankYouPage = (props) => {
-  return (
-    <div className="ThankYouPage">
-      <div className="ThankYouPage_Content">
-        <h1>Thank You!</h1>
-        <p>
-          Bless you for all you're doing to care for your patients and each
-          other during this pandemic.
-          <br /> <br />
-          Please share this project with your colleagues, and stay in touch!
-        </p>
+	return (
+		<div className="ThankYouPage">
+			<div className="ThankYouPage_Content">
+				<h1>Thank You!</h1>
+				<p>
+					Bless you for all you're doing to care for your patients and each other during this
+					pandemic.
+					<br /> <br />
+					Please share this project with your colleagues, and stay in touch!
+				</p>
 
-        <div className="socials">
-          <a href="https://www.facebook.com/flo.nightingale.3344">
-            <i className="fa fa-facebook-f"></i>
-          </a>
-          <a href="https://twitter.com/F_NightingaleRN">
-            <i className="fa fa-twitter"></i>
-          </a>
-          <a href="https://www.instagram.com/FloNightingaleRN/">
-            <i className="fa fa-instagram"></i>
-          </a>
-          <a href="mailto:flo@codefordayton.org?subject=Flo's Whistle: Pandemic">
-            <i className="far fa-envelope"></i>
-          </a>
-          <p>flo@codefordayton.org</p>
-        </div>
+				<div className="socials">
+					<a href="https://www.facebook.com/flo.nightingale.3344">
+						<i className="fa fa-facebook-f"></i>
+					</a>
+					<a href="https://twitter.com/F_NightingaleRN">
+						<i className="fa fa-twitter"></i>
+					</a>
+					<a href="https://www.instagram.com/FloNightingaleRN/">
+						<i className="fa fa-instagram"></i>
+					</a>
+					<a href="mailto:flo@codefordayton.org?subject=Flo's Whistle: Pandemic">
+						<i className="far fa-envelope"></i>
+					</a>
+					<p>flo@codefordayton.org</p>
+				</div>
 
-        <div className="buttons">
-          <Link to="/results" className="UtilLink">
-            see results dashboard
-          </Link>
-        </div>
-      </div>
-      <p className="CodeForDayton">
-        <strong>Flo's Whistle: Pandemic</strong> <br />
-        is a Code for Dayton project. We're an independent collaboration of
-        volunteer nurses, physicians, attorneys, writers, designers, journalists
-        and developers from the Code for America Brigade community. ©2020
-      </p>
-    </div>
-  );
+				<div className="buttons">
+					<Link to="/results" className="UtilLink">
+						see results dashboard
+					</Link>
+				</div>
+			</div>
+			<AboutCodeForDayton />
+		</div>
+	);
 };
 
 export default ThankYouPage;
