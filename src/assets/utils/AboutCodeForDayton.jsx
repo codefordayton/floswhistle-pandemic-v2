1da7bbc4f215b58850c1ae8afe4620201014c00c
Melanie <melanie.mazanec@gmail.com>
2020-05-25 14:57:17 -0400

    Add eslint and prettier, format code

--- src/assets/utils/AboutCodeForDayton.jsx
+++ src/assets/utils/AboutCodeForDayton.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export default () => (
-	<p>
-		<strong>Flo's Whistle: Pandemic</strong> is a Code for Dayton project. We're an independent
-		collaboration of volunteer developers, nurses, physicians, attorneys, writters, designers, and
-		journalists from the Code for America Brigade community. ©2020
-	</p>
+  <p>
+    <strong>Flo's Whistle: Pandemic</strong> is a{' '}
+    <a href="https://codefordayton.org/" target="_blank" rel="noopener noreferrer">
+      Code for Dayton
+    </a>{' '}
+    project. We're an independent collaboration of volunteer developers, nurses, physicians,
+    attorneys, writters, designers, and journalists from the Code for America Brigade community.
+    ©2020
+  </p>
 );
--- src/containers/thankyoupage/ThankYouPage.js
+++ src/containers/thankyoupage/ThankYouPage.js
@@ -4,42 +4,42 @@ import AboutCodeForDayton from '../../assets/utils/AboutCodeForDayton';
 import './ThankYouPage.css';
 
 const ThankYouPage = (props) => {
-	return (
-		<div className="ThankYouPage">
-			<div className="ThankYouPage_Content">
-				<h1>Thank You!</h1>
-				<p>
-					Bless you for all you're doing to care for your patients and each other during this
-					pandemic.
-					<br /> <br />
-					Please share this project with your colleagues, and stay in touch!
-				</p>
+  return (
+    <div className="ThankYouPage">
+      <div className="ThankYouPage_Content">
+        <h1>Thank You!</h1>
+        <p>
+          Bless you for all you're doing to care for your patients and each other during this
+          pandemic.
+          <br /> <br />
+          Please share this project with your colleagues, and stay in touch!
+        </p>
 
-				<div className="socials">
-					<a href="https://www.facebook.com/flo.nightingale.3344">
-						<i className="fa fa-facebook-f"></i>
-					</a>
-					<a href="https://twitter.com/F_NightingaleRN">
-						<i className="fa fa-twitter"></i>
-					</a>
-					<a href="https://www.instagram.com/FloNightingaleRN/">
-						<i className="fa fa-instagram"></i>
-					</a>
-					<a href="mailto:flo@codefordayton.org?subject=Flo's Whistle: Pandemic">
-						<i className="far fa-envelope"></i>
-					</a>
-					<p>flo@codefordayton.org</p>
-				</div>
+        <div className="socials">
+          <a href="https://www.facebook.com/flo.nightingale.3344">
+            <i className="fa fa-facebook-f"></i>
+          </a>
+          <a href="https://twitter.com/F_NightingaleRN">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="https://www.instagram.com/FloNightingaleRN/">
+            <i className="fa fa-instagram"></i>
+          </a>
+          <a href="mailto:flo@codefordayton.org?subject=Flo's Whistle: Pandemic">
+            <i className="far fa-envelope"></i>
+          </a>
+          <p>flo@codefordayton.org</p>
+        </div>
 
-				<div className="buttons">
-					<Link to="/results" className="UtilLink">
-						see results dashboard
-					</Link>
-				</div>
-			</div>
-			<AboutCodeForDayton />
-		</div>
-	);
+        <div className="buttons">
+          <Link to="/results" className="UtilLink">
+            see results dashboard
+          </Link>
+        </div>
+      </div>
+      <AboutCodeForDayton />
+    </div>
+  );
 };
 
 export default ThankYouPage;
